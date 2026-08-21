964458fc27d4dc0645358ad8e02ab96eada964ab
Juna Lnx <itujun7@gmail.com>
2026-08-21 17:14:03 +0700

    BE Auth - Login 1

---
+++ src/module/auth/dto/auth-login.dto.ts
@@ -0,0 +1,60 @@
+import { Expose, Type } from 'class-transformer';
+
+class RoleResponse {
+    @Expose()
+    id: number;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    key: string;
+
+    @Expose()
+    @Type(() => PermissionResponse)
+    permission: PermissionResponse[];
+}
+
+class PermissionResponse {
+    @Expose()
+    id: number;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    key: string;
+
+    @Expose()
+    resource: string;
+}
+
+class UserResponse {
+    @Expose()
+    id: number;
+
+    @Expose()
+    email: string;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    avatar: string;
+
+    @Expose()
+    phoneNumber: string;
+
+    @Expose()
+    @Type(() => RoleResponse)
+    roles: RoleResponse[];
+}
+
+export class AuthLoginResponse {
+    @Expose()
+    accessToken: string;
+
+    @Expose()
+    @Type(() => UserResponse)
+    user: UserResponse;
+}
