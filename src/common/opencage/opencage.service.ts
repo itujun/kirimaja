0b88378583e25e079e841cc6599f01a270b882c1
Juna Lnx <itujun7@gmail.com>
2026-08-22 23:23:55 +0700

    BE Data Master - User Addresses 1

---
+++ src/common/opencage/opencage.service.ts
@@ -0,0 +1,41 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { envSchema } from 'src/config/env.schema';
+
+@Injectable()
+export class OpenCageService {
+    async geocode(address: string): Promise<{ lat: number; lng: number }> {
+        const apiKey = envSchema.OPENCAGE_API_KEY;
+        if (!apiKey) {
+            throw new BadRequestException(
+                'API key not found in environment variables',
+            );
+        }
+
+        try {
+            const response = await axios.get(
+                'https://api.opencagedata.com/geocode/v1/json',
+                {
+                    params: {
+                        q: address,
+                        key: apiKey,
+                        limit: 1,
+                    },
+                },
+            );
+            const result = response.data.results[0]?.geometry;
+            if (!result) {
+                throw new BadRequestException(
+                    'No results found for provided address',
+                );
+            }
+            return {
+                lat: result.lat,
+                lng: result.lng,
+            };
+        } catch (error) {
+            console.error('OpenCage geocoding error:', error);
+            throw new BadRequestException('OpenCage geocoding error');
+        }
+    }
+}
--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -17,6 +17,10 @@ export const envSchema = z.object({
         .string({ required_error: 'JWT_SECRET_KEY wajib diisi di .env' })
         .min(16, 'JWT_SECRET_KEY minimal 16 karakter demi keamanan'),
     JWT_EXPIRES_IN: z.coerce.number().int().positive().default(86400), // 1 day
-});
 
+    // OpenCageData
+    OPENCAGE_API_KEY: z
+        .string({ required_error: 'OPENCAGE_API_KEY wajib diisi di .env' })
+        .min(1, 'OPENCAGE_API_KEY tidak boleh kosong'),
+});
 export type Env = z.infer<typeof envSchema>;
